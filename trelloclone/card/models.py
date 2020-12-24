1c634fe3f8edb0a9861b1136c846701017c43dc7
icycle0409 <icycle0409@snu.ac.kr>
2020-12-24 17:50:01 +0900

    runserver success

--- trelloclone/card/models.py
+++ trelloclone/card/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from list.models import List
-from board.models import Board
+
 
 class Card(models.Model):
     name = models.CharField(max_length=30, db_index=True, null=False)
@@ -11,13 +10,13 @@ class Card(models.Model):
     is_head = models.BooleanField(default=False)
 
     creator = models.ForeignKey(User, related_name='card_creator', on_delete=models.DO_NOTHING)
-    list = models.ForeignKey(List, related_name='card_list', on_delete=models.CASCADE)
-    board = models.ForeignKey(Board, related_name='card_board', on_delete=models.CASCADE)
+    list = models.ForeignKey('list.List', related_name='card_list', on_delete=models.CASCADE)
+    board = models.ForeignKey('board.Board', related_name='card_board', on_delete=models.CASCADE)
     prev = models.ForeignKey('self', related_name='next', on_delete=models.DO_NOTHING)
 
 class UserCard(models.Model):
-    user = models.ForeignKey(User, related_name='user_card', on_delete=models.CASCADE)
-    card = models.ForeignKey(Card, related_name='user_card', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_card', on_delete=models.DO_NOTHING)
+    card = models.ForeignKey(Card, related_name='user_card', on_delete=models.DO_NOTHING)
 
 
 
