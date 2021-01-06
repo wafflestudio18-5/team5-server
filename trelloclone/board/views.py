e8750c2f5a364238f19b3f31a1783b8959eceda9
GanziDaeyong <GanziDaeyong@github.com>
2021-01-06 08:57:03 +0000

    ec2-auto-commiting

--- trelloclone/board/views.py
+++ trelloclone/board/views.py
@@ -11,7 +11,7 @@ from user.serializers import UserSerializer
 from django.contrib.auth.models import User
 
 class BoardViewSet(viewsets.GenericViewSet):
-    pagination_class = StandardResultsSetPagination
+    
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
 
