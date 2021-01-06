8118537eb25d3be6504702a828538a63f5a13580
GanziDaeyong <GanziDaeyong@github.com>
2021-01-06 08:31:49 +0000

    ec2-auto-commiting

--- trelloclone/trelloclone/settings.py
+++ trelloclone/trelloclone/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
