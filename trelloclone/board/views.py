21520140fb03db0dc178e3c2ff7fd2574d05e7b6
icycle0409 <icycle0409@snu.ac.kr>
2021-01-08 17:38:33 +0900

    star debug 2

--- trelloclone/board/views.py
+++ trelloclone/board/views.py
@@ -174,6 +174,6 @@ class BoardViewSet(viewsets.GenericViewSet):
             ub.star=False
             ub.save()
         board.save()
-        return Response(self.get_serializer(board).data, status=status.HTTP_200_OK)
+        return Response(self.get_serializer(board,context={'request': request}).data, status=status.HTTP_200_OK)
 
 # Create your views here.
