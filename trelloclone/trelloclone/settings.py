1509de494754e5e8b8bbfec5aa5a2cdd554aad60
GanziDaeyong <iwin1203@naver.com>
2021-01-09 03:11:13 +0900

    temp

--- trelloclone/trelloclone/settings.py
+++ trelloclone/trelloclone/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
 if DEBUG_TOOLBAR:
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
-    INTERNAL_IPS = ('127.0.0.1',)
+    INTERNAL_IPS = ('127.0.0.1','')
 
 ROOT_URLCONF = 'trelloclone.urls'
 
